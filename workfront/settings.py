20c07d3c185955a861f020fca7647861779dd68d
oladeindepaul <101461115+oladeindepaul@users.noreply.github.com>
2022-10-21 16:48:10 +0100

    Update settings.py

--- workfront/settings.py
+++ workfront/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u32i!b7zxq_%4n6mdr15t&xt(cl1$++ilph3ukw)dz51^+kyb='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
